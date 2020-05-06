3abd4ba86b24cf8abdb584ad3470e56fee3e6387
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-05-06 17:06:07 -0500

    Setup initial script with general game idea

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Fruit-Slice-Game
+# Fruit slicer game made for fun using HTML, CSS and JQuery
---
+++ game.js
@@ -0,0 +1,29 @@
+$(function(){
+
+});
+
+//click on start-reset button
+	//check if playing
+		//if yes
+			//reload page
+		//if not
+			//show trials left
+			//change button text to "reset game"
+			//(1)create a random fruit
+			//(2)move food down by 1 step every few seconds
+				//check if fruit is very low(disappearing)
+					//if no, repeat (2)
+					//else, check if any trials left
+						//if yes
+							//remove a heart 
+							//then repeat (1)
+						//else
+							//show gameover box
+							//change button to display "start game"
+
+
+
+
+//slicing fruit
+	//play some background sound(confirms fruit as cut)
+	//explode the fruit
