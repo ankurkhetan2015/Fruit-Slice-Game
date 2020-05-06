3475fc4c5351151b20d20194aba78172a7232aa9
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-05-06 18:05:48 -0500

    Added hearts for the trials and toggled b/w Start/Reset

--- game.js
+++ game.js
@@ -1,14 +1,41 @@
-$(function(){
+var playing = false;
+var currentScore;
+var trialsLeft;
 
+$(function()
+{	
+	//click on start-reset button
+	$("#startreset").click(function()
+	{
+		//check if playing
+		if(playing == true)
+		{			
+			//reload page
+			location.reload();
+		}
+		
+		// if not playing
+		else
+		{			
+			playing = true;
+			
+			//set score to 0
+			currentScore = 0;
+			$("#scorevalue").html(currentScore);
+			
+			//show trials left
+			$("#trialsleft").show();
+			trialsLeft = 3;
+			addHearts();
+
+			//change button text to "reset game"
+			$("#startreset").html("Reset Game");
+		}
+	});
 });
 
-//click on start-reset button
-	//check if playing
-		//if yes
-			//reload page
-		//if not
-			//show trials left
-			//change button text to "reset game"
+
+			
 			//(1)create a random fruit
 			//(2)move food down by 1 step every few seconds
 				//check if fruit is very low(disappearing)
@@ -27,3 +54,12 @@ $(function(){
 //slicing fruit
 	//play some background sound(confirms fruit as cut)
 	//explode the fruit
+
+
+function addHearts()
+{	
+	for(i = 0; i < trialsLeft; i++)
+	{
+		$("#trialsleft").append('<img class="lives" src="images/heart.png">');
+	}
+}
