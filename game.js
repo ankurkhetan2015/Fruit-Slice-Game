955324f5e92aec70e819502a92097e099ea2e3cb
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-05-06 20:09:52 -0500

    Added support for lives left, and implemented game over situation

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var playing = false;
 var currentScore;
 var trialsLeft;
+var step;
+var action;
 var allfruits = ['apples', 'banana', 'cherries', 'coconut', 'kiwi', 'lemons', 'mango', 'orange', 'pears', 'strawberries', 'tomatoes', 'watermelon'];
-var numFruits = allfruits.length - 1 //index of first fruit is 0
+var numFruits = allfruits.length - 1; //index of first fruit is 0
 
 $(function()
 {	
@@ -40,7 +42,7 @@ $(function()
 });
 
 
-			
+			//define random step
 			//(2)move food down by 1 step every few seconds
 				//check if fruit is very low(disappearing)
 					//if no, repeat (2)
@@ -63,6 +65,7 @@ $(function()
 //funtion to a lives remaining in a game
 function addHearts()
 {	
+	$("#trialsleft").empty();
 	for(i = 0; i < trialsLeft; i++)
 	{
 		$("#trialsleft").append('<img class="lives" src="images/heart.png">');
@@ -72,14 +75,73 @@ function addHearts()
 //function to start the game (send fruits)
 function startGame()
 {
-	$("#fruits").show();
-	//choose a random fruit
-	chooseFruit();
-	$("#fruits").css({'left' : Math.round(550*Math.random()), 'top' : -50});
+	generateFruit();
+
+	//move food down by 1 step every few milliseconds, 10 right now
+	action = setInterval(function()
+	{	
+		//move food by a step
+		$("#fruits").css('top', $("#fruits").position().top + step);
+
+		//check if fruit is very low(disappeared)
+		if($("#fruits").position().top > $("#gamebox").height())
+		{
+			//check if any trials left
+			if(trialsLeft > 1)
+			{
+				generateFruit();
+
+				//reduce trial by 1 (remove a heart)
+				trialsLeft --;
+				//populated right number of hearts in trialsleft box
+				addHearts();
+			}
+			else
+			{
+				playing = false;
+
+				//change button text to "start game"
+				$("#startreset").html("Start Game");
+				
+				//show gameover box
+				$("#gameOver").show();
+				$("#gameOver").html('<p>Game Over!!!</p><p>Your Score is: ' + currentScore + '</p>');
+
+				//stop the game
+				stopGame();
+			}
+		}
+
+	}, 10);
+
+
+
 }
 
 //function to generate a random fruit to send
 function chooseFruit()
 {
 	$("#fruits").attr('src', 'images/' + allfruits[Math.round(numFruits * Math.random())] + '.png');
+}
+
+//decides position and speed of falling of a random fruit
+function generateFruit()
+{
+	$("#fruits").show();
+	
+	//choose a random fruit
+	chooseFruit();
+	
+	//random fruit position
+	$("#fruits").css({'left' : Math.round(350*Math.random()), 'top' : -50});
+
+	//generate random step, changing step
+	step = 1 + Math.round(5*Math.random());
+}
+
+//stop the game and stop dropping the fruits
+function stopGame()
+{
+	clearInterval(action);
+	$("#fruits").hide();
 }
