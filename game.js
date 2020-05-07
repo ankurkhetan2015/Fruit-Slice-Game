0bf86fdb1504b084b7344a1d73bd72d66ea78be1
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-05-06 19:25:43 -0500

    Random fruit generation implemented

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 var playing = false;
 var currentScore;
 var trialsLeft;
+var allfruits = ['apples', 'banana', 'cherries', 'coconut', 'kiwi', 'lemons', 'mango', 'orange', 'pears', 'strawberries', 'tomatoes', 'watermelon'];
+var numFruits = allfruits.length - 1 //index of first fruit is 0
 
 $(function()
 {	
@@ -30,13 +32,15 @@ $(function()
 
 			//change button text to "reset game"
 			$("#startreset").html("Reset Game");
+
+			//start the game
+			startGame();
 		}
 	});
 });
 
 
 			
-			//(1)create a random fruit
 			//(2)move food down by 1 step every few seconds
 				//check if fruit is very low(disappearing)
 					//if no, repeat (2)
@@ -56,10 +60,26 @@ $(function()
 	//explode the fruit
 
 
+//funtion to a lives remaining in a game
 function addHearts()
 {	
 	for(i = 0; i < trialsLeft; i++)
 	{
 		$("#trialsleft").append('<img class="lives" src="images/heart.png">');
 	}
+}
+
+//function to start the game (send fruits)
+function startGame()
+{
+	$("#fruits").show();
+	//choose a random fruit
+	chooseFruit();
+	$("#fruits").css({'left' : Math.round(550*Math.random()), 'top' : -50});
+}
+
+//function to generate a random fruit to send
+function chooseFruit()
+{
+	$("#fruits").attr('src', 'images/' + allfruits[Math.round(numFruits * Math.random())] + '.png');
 }
